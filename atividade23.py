e43e5c5c5dfc4bd5d18501c483710baccf9be677
gabriellevims <gabriellevi2104@gmail.com>
2023-10-31 21:35:55 -0300

    coreção do código

--- atividade23.py
+++ atividade23.py
@@ -1,4 +1,11 @@
 # Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
 # e que se encontram no intervalo de 1 até 500.
 print("Quais saõ os números múltiplos de 3?")
-print(list(range(3,500,3)))
+soma = 0
+num = []
+for i in range(1,501):
+    if i % 3 == 0:
+     i
+    soma += i
+print("soma dos múltiplos de 3",soma)
+      
